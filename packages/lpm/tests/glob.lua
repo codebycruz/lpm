ce95c34747c72ff84da6babe394b8a6cb2829793
David Cruz <david@codebycruz.com>
2025-12-31 04:13:00 -0800

    Get the busted poc working
    It's now a dev dependency and runs the tests.

--- packages/lpm/tests/glob.lua
+++ packages/lpm/tests/glob.lua
@@ -1,12 +1,39 @@
-local fs = require("fs")
-local path = require("path")
+local function cb()
+	local fs = require("fs")
+	local path = require("path")
 
-require("busted.runner")()
+	it("should match glob pattern", function()
+		assert.is_truthy(string.match("foo/whatever.lua", fs.globToPattern("**.lua")))
+	end)
 
-error("stop" .. tostring(describe))
-assert(string.match("foo/whatever.lua", fs.globToPattern("**.lua")))
+	it("should calculate relative path", function()
+		local base = "/home/user/project"
+		local relative = "/home/user/project/file.lua"
+		assert.is_equal("file.lua", path.relative(base, relative))
+	end)
+end
 
-local base = "/home/user/project"
-local relative = "/home/user/project/file.lua"
+local busted = require("busted.core")()
+require("busted")(busted)
+busted.wrap(cb)
+cb()
 
-assert(path.relative(base, relative) == "file.lua")
+local fails, errors = 0, 0
+busted.subscribe({ 'error' }, function(element, parent, message)
+	errors = errors + 1
+	io.stderr:write(message)
+	return nil, true
+end)
+
+busted.subscribe({ 'failure' }, function(element, parent, message)
+	fails = fails + 1
+	io.stderr:write(message)
+	return nil, true
+end)
+
+local run = require("busted.execute")(busted)
+run(1, {})
+
+if fails > 0 or errors > 0 then
+	os.exit(1)
+end
