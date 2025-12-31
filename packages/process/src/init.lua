7abb0fe42b489daec0732ba7423cc185f747460c
David Cruz <david@codebycruz.com>
2025-12-30 20:37:14 -0800

    Didn't actually commit the process library..
    This library intends to replace all manual use of the os and io library
    functions in the fs and other libraries. It does escaping and env vars
    safely.

---
+++ packages/process/src/init.lua
@@ -0,0 +1,78 @@
+local process = {}
+
+local path = require("path")
+
+local isWindows = path.separator == "\\"
+
+---@param arg string
+local function escape(arg)
+	if isWindows then
+		return '"' .. string.gsub(arg, '"', '\\"') .. '"'
+	else
+		return "'" .. string.gsub(arg, "'", "'\\''") .. "'"
+	end
+end
+
+---@class process.SpawnOptions
+---@field cwd string?
+---@field env table<string, string>?
+
+---@param name string
+---@param args string[]
+---@param options process.SpawnOptions?
+local function formatCommand(name, args, options)
+	local parts = { escape(name) }
+	for i, arg in ipairs(args) do
+		parts[i + 1] = escape(arg)
+	end
+	local command = table.concat(parts, " ")
+
+	if options and options.cwd then
+		command = "cd " .. escape(options.cwd) .. " && " .. command
+	end
+
+	if options and options.env then
+		local parts = {}
+		for k, v in pairs(options.env) do
+			if isWindows then
+				parts[#parts + 1] = "set " .. k:match("^[%w_]+$") .. "=" .. escape(v) .. "&&"
+			else
+				parts[#parts + 1] = k:match("^[%w_]+$") .. "=" .. escape(v)
+			end
+		end
+
+		command = table.concat(parts, " ") .. " " .. command
+	end
+
+	return command
+end
+
+---@param name string
+---@param args string[]
+---@param options process.SpawnOptions?
+---@return boolean? # Success
+---@return string # Output
+function process.exec(name, args, options)
+	local command = formatCommand(name, args, options)
+	command = command .. " 2>&1" -- Redirect stderr to stdout
+
+	local handle = io.popen(command, "r")
+	if not handle then
+		error("Failed to start process: " .. command)
+	end
+
+	local output = handle:read("*a")
+	local success = handle:close()
+
+	return success, output
+end
+
+---@param name string
+---@param args string[]
+---@param options process.SpawnOptions?
+function process.spawn(name, args, options)
+	local command = formatCommand(name, args, options)
+	return os.execute(command)
+end
+
+return process
