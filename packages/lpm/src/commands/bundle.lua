084b40f60d3809b24f5aa3c6bbe64c74607b23c4
David Cruz <david@codebycruz.com>
2026-01-02 20:19:18 -0800

    Default to package name for compile binary
    By default it creates it at the package directory with the same name
    instead of needing an --outfile.

--- packages/lpm/src/commands/bundle.lua
+++ packages/lpm/src/commands/bundle.lua
@@ -1,10 +1,15 @@
 local ansi = require("ansi")
+local path = require("path")
+
+local Package = require("lpm.package")
 
 ---@param args clap.Args
 local function bundle(args)
 	local outFile = args:key("outfile", "string")
+
+	local pkg = Package.open()
 	if not outFile then
-		error("Please specify an output file using --outfile")
+		outFile = path.join(pkg:getDir(), pkg:getName() .. "-bundled.lua")
 	end
 
 	print(ansi.colorize(ansi.red, "Bundling is not yet implemented."))
--- packages/lpm/src/commands/compile.lua
+++ packages/lpm/src/commands/compile.lua
@@ -1,26 +1,27 @@
 local ansi = require("ansi")
 local fs = require("fs")
 local process = require("process")
+local path = require("path")
 
 local Package = require("lpm.package")
 
 ---@param args clap.Args
 local function compile(args)
 	local outFile = args:key("outfile", "string")
+
+	local pkg = Package.open()
 	if not outFile then
-		error("Please specify an output file using --outfile")
+		outFile = path.join(pkg:getDir(), pkg:getName())
 	end
 
-	if process.platform == "win32" then
-		if string.sub(outFile, -4) ~= ".exe" then
-			outFile = outFile .. ".exe"
-		end
+	if process.platform == "win32" and string.sub(outFile, -4) ~= ".exe" then
+		outFile = outFile .. ".exe"
 	end
 
-	local executable = Package.open():compile()
+	local executable = pkg:compile()
 	fs.move(executable, outFile)
 
-	print(ansi.colorize(ansi.green, "Bundle created: " .. outFile))
+	print(ansi.colorize(ansi.green, "Executable created: " .. outFile))
 end
 
 return compile
