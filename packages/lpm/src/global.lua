36a681eb5591346e689c042d7822d29f6571f87d
David Cruz <david@codebycruz.com>
2026-01-23 19:02:34 -0800

    chore: bump to 0.4.1

--- packages/lpm/src/global.lua
+++ packages/lpm/src/global.lua
@@ -4,7 +4,7 @@ local fs = require("fs")
 local path = require("path")
 local process = require("process")
 
-global.currentVersion = "0.4.0"
+global.currentVersion = "0.4.1"
 
 function global.getDir()
 	local home = os.getenv("HOME") or os.getenv("USERPROFILE")
