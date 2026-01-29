239610bf33896aca5709f1db82dcf7e4b93eba9a
David Cruz <david@codebycruz.com>
2026-01-29 13:50:51 -0800

    refactor(clap)!: new clap api
    It lazily parses options to avoid ambiguous situations, ie supporting
    both `--foo bar` and `--foo`. now there's Args:option(k) and
    Args:flag(k) respectively for whichever case you want.
    
    Also, now it's untyped because implicit casting could lead to footguns.
    Additionally, parsing arguments returns the index at which the argument
    was at.
    
    This is specifically useful (and going to be used by lpm) for
    determining -- and parsing the arguments afterward as non-options.

--- packages/clap/src/init.lua
+++ packages/clap/src/init.lua
@@ -1,107 +1,92 @@
 local clap = {}
 
 ---@class clap.Args
----@field private kvs table<string, any>
----@field private args any[]
+---@field private raw string[]
 local Args = {}
 Args.__index = Args
 
----@generic T
----@param typeName `T`
----@return T?
-function Args:pop(typeName)
-	local ty = type(self.args[1])
-	if ty == type(typeName) then
-		return table.remove(self.args, 1)
-	end
+---@return string?
+function Args:pop()
+	return table.remove(self.raw, 1)
 end
 
----@generic T
----@param key string
----@param typeName `T`
----@return T?
-function Args:key(key, typeName)
-	local val = self.kvs[key]
-	if val ~= nil then
-		if type(val) == type(typeName) then
-			return val
-		end
-	end
-end
-
----@return number
-function Args:count()
-	return #self.args
-end
-
----@param key string
----@return boolean
-function Args:has(key)
-	return self.kvs[key] ~= nil
-end
-
----@param rawArgs string[]
----@return clap.Args
-function clap.parse(rawArgs)
-	---@type table<string, any>
-	local kvs = {}
-
-	local ptr, len = 1, #rawArgs
-
-	---@param val string
-	local function parseValue(val)
-		if val == "true" then
-			return true
-		elseif val == "false" then
-			return false
-		elseif tonumber(val) then
-			return tonumber(val)
-		else
-			return val
-		end
-	end
-
-	---@param arg string
-	local function parseArg(arg)
-		if string.sub(arg, 1, 2) == "--" then
-			local eq = string.find(arg, "=", 1, true)
+---@param desiredKey string
+---@return string? val
+---@return number? beforePos # New position before the option key
+function Args:option(desiredKey)
+	for i, raw in ipairs(self.raw) do
+		if string.sub(raw, 1, 2) == "--" then
+			local eq = string.find(raw, "=", 1, true)
 			if eq then
-				local key = string.sub(arg, 3, eq - 1)
-				local value = string.sub(arg, eq + 1)
+				local key = string.sub(raw, 3, eq - 1)
+				local value = string.sub(raw, eq + 1)
 
-				kvs[key] = parseValue(value)
+				if key == desiredKey then
+					table.remove(self.raw, i) -- Remove the option key
+					return value, i - 1
+				end
 			else
-				local key = string.sub(arg, 3)
-				local nextArg = rawArgs[ptr]
-				if nextArg ~= nil and string.sub(nextArg, 1, 1) ~= "-" then
-					ptr = ptr + 1
-					kvs[key] = parseValue(nextArg)
-				else
-					kvs[key] = true
+				local key = string.sub(raw, 3)
+				if key == desiredKey and self.raw[i + 1] ~= nil then
+					local _key = table.remove(self.raw, i)
+					return table.remove(self.raw, i), i - 2
 				end
 			end
 
 			return nil
 		end
-
-		return parseValue(arg)
 	end
+end
 
-	local args = {}
-	while ptr <= len do
-		local rawArg = rawArgs[ptr]
-		ptr = ptr + 1
+---@param start number?
+function Args:drain(start)
+	if start then
+		local args = {}
+		for i = start, #self.raw do
+			args[#args + 1] = self.raw[i]
+		end
 
-		local parsed = parseArg(rawArg)
-		if parsed ~= nil then
-			args[#args + 1] = parsed
+		for i = #self.raw, start, -1 do
+			table.remove(self.raw, i)
+		end
+
+		return args
+	else
+		local args = self.raw
+		self.raw = {}
+		return args
+	end
+end
+
+---@return number
+function Args:count()
+	return #self.raw
+end
+
+---@param desiredKey string
+---@return boolean
+---@return number? pos
+function Args:flag(desiredKey)
+	for i, arg in ipairs(self.raw) do
+		if string.sub(arg, 1, 2) == "--" then
+			local eq = string.find(arg, "=", 1, true)
+			if eq == nil then
+				local key = string.sub(arg, 3)
+				if key == desiredKey then
+					table.remove(self.raw, i)
+					return true, i - 1
+				end
+			end
 		end
 	end
 
-	return setmetatable({
-		args = args,
-		kvs = kvs,
-	}, Args)
+	return false, nil
+end
+
+---@param rawArgs string[]
+---@return clap.Args
+function clap.parse(rawArgs)
+	return setmetatable({ raw = rawArgs }, Args)
 end
 
 return clap
