4afd57e4c251c7dfedd12a5d40fa78d8cd16a92d
David Cruz <david@codebycruz.com>
2026-01-29 13:56:30 -0800

    feat: better `lpm run`
    - Now when not given a path, it defaults to the package's init file.
    
    - You can pass -- <args> to give the program arguments. For example `lpm
      run -- test` will run the lpm test suite inside of lpm.
    
    Resolves #22

--- packages/lpm/src/commands/run.lua
+++ packages/lpm/src/commands/run.lua
@@ -1,13 +1,29 @@
 local Package = require("lpm.package")
 
+local path = require("path")
+
 ---@param args clap.Args
 local function run(args)
-	local file = assert(args:pop("string"), "Usage: lpm run whatever.lua")
-
 	local pkg = Package.open()
 	pkg:build()
 
-	local ok, err = pkg:runScript(file)
+	local scriptArgs = {}
+	local scriptPath = nil ---@type string?
+
+	local dash, dashPos = args:flag("")
+	if dash then
+		if dashPos ~= 0 then
+			scriptPath = args:pop()
+		end
+
+		scriptArgs = args:drain(dashPos)
+	else
+		scriptPath = args:pop()
+	end
+
+	scriptPath = scriptPath or path.join(pkg:getTargetDir(), "init.lua")
+
+	local ok, err = pkg:runScript(scriptPath, scriptArgs)
 	if not ok then
 		error("Failed to run script: " .. err)
 	end
