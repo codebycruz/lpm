d60a141cdb1fd619a20fb764a1831f13dfc90dd3
David Cruz <david@codebycruz.com>
2026-02-05 23:42:33 -0800

    chore: bump to 0.5.1

--- packages/lpm-core/src/global.lua
+++ packages/lpm-core/src/global.lua
@@ -4,7 +4,7 @@ local fs = require("fs")
 local path = require("path")
 local process = require("process")
 
-global.currentVersion = "0.5.0"
+global.currentVersion = "0.5.1"
 
 function global.getDir()
 	local home = os.getenv("HOME") or os.getenv("USERPROFILE")
