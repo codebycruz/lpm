182add0be5fbbfde495e01fe7adfe95601c60be6
David Cruz <david@codebycruz.com>
2026-02-14 01:35:52 -0800

    feat(initialize): create gitignore and luarc.json on init

--- packages/lpm-core/src/package/initialize.lua
+++ packages/lpm-core/src/package/initialize.lua
@@ -21,6 +21,50 @@ local function initPackage(dir)
 		}
 	]]))
 
+	local idealGitignore = util.dedent([[
+		/lpm_modules/
+		/target/ # Reserved for future use
+		/lpm.lock # Reserved for future use
+		/lpm-lock.json
+	]])
+
+	local gitignorePath = path.join(dir, ".gitignore")
+	if not fs.exists(gitignorePath) then
+		fs.write(gitignorePath, idealGitignore)
+	else -- Try to append to it
+		local content = fs.read(gitignorePath)
+		if not content then
+			error("Failed to read existing .gitignore at: " .. gitignorePath)
+		end
+
+		if not string.find(content, "/lpm_modules/", 1, true) then
+			content = content .. "\n" .. idealGitignore
+			fs.write(gitignorePath, content)
+		end
+	end
+
+	local luarcPath = path.join(dir, ".luarc.json")
+	if not fs.exists(luarcPath) then
+		fs.write(luarcPath, util.dedent([[
+			{
+				"$schema": "https://raw.githubusercontent.com/sumneko/vscode-lua/master/setting/schema.json",
+				"diagnostics": {
+					"disable": [
+						"duplicate-doc-field",
+						"duplicate-doc-field",
+						"duplicate-index",
+						"duplicate-set-field",
+						"duplicate-doc-alias"
+					]
+				},
+				"runtime": {
+					"version": "LuaJIT",
+					"path": ["./lpm_modules/?.lua", "./lpm_modules/?/init.lua"]
+				}
+			}
+		]]))
+	end
+
 	local package = Package.open(dir)
 	if not package then
 		error("Failed to initialize package at directory: " .. dir)
