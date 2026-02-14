6f464909614b79f4f0ef59c79da6bc19c976069c
David Cruz <david@codebycruz.com>
2026-02-13 23:01:53 -0800

    refactor: split up install code further

--- packages/lpm-core/src/package/install.lua
+++ packages/lpm-core/src/package/install.lua
@@ -19,6 +19,42 @@ local function installDependency(package, dependency)
 	dependency:build(destinationPath)
 end
 
+--- Gets a proper lpm.Package instance from dependency info.
+--- For git dependencies, this will clone it to the global git cache.
+--- For path dependencies, this will resolve the path and load the package from there.
+---@param name string
+---@param depInfo lpm.Config.Dependency
+---@param relativeTo string
+local function dependencyToPackage(name, depInfo, relativeTo)
+	if depInfo.git then
+		local repoDir = global.getOrInitGitRepo(name, depInfo.git)
+
+		for _, config in ipairs(fs.scan(repoDir, "**" .. path.separator .. "lpm.json")) do
+			local parentDir = path.join(repoDir, path.dirname(config))
+
+			local gitDependencyPackage, err = Package.open(parentDir)
+			if not gitDependencyPackage then
+				error("Failed to load git dependency package for: " .. name .. "\nError: " .. err)
+			end
+
+			return gitDependencyPackage
+		end
+
+		error("No lpm.json found in git repository for dependency: " .. name)
+	elseif depInfo.path then
+		local normalized = path.normalize(depInfo.path)
+		local localPackage, err = Package.open(path.resolve(relativeTo, normalized))
+
+		if not localPackage then
+			error("Failed to load local dependency package for: " .. name .. "\nError: " .. err)
+		end
+
+		return localPackage
+	else
+		error("Unsupported dependency type for: " .. name)
+	end
+end
+
 ---@param package lpm.Package
 ---@param dependencies table<string, lpm.Config.Dependency>?
 ---@param relativeTo string? # Directory to resolve relative paths from
@@ -34,28 +70,12 @@ local function installDependencies(package, dependencies, relativeTo)
 	for name, depInfo in pairs(dependencies) do
 		local destinationPath = path.join(modulesDir, name)
 		if fs.exists(destinationPath) then
+			-- TODO: replace with update logic..
 			goto skip
 		end
 
-		if depInfo.git then
-			local repoDir = global.getOrInitGitRepo(name, depInfo.git)
-
-			for _, config in ipairs(fs.scan(repoDir, "**" .. path.separator .. "lpm.json")) do
-				local parentDir = path.join(repoDir, path.dirname(config))
-				local gitDependencyPackage = Package.open(parentDir)
-
-				if gitDependencyPackage:getName() == name then
-					installDependency(package, gitDependencyPackage)
-				end
-			end
-		elseif depInfo.path then
-			local normalized = path.normalize(depInfo.path)
-			local localPackage = Package.open(path.resolve(relativeTo, normalized))
-
-			installDependency(package, localPackage)
-		else
-			error("Unsupported dependency type for: " .. name)
-		end
+		local dependencyPackage = dependencyToPackage(name, depInfo, relativeTo)
+		installDependency(package, dependencyPackage)
 
 		::skip::
 	end
