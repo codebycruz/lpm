36e0c35c7286774361f19fd4b109e08a9a7914ed
David Cruz <david@codebycruz.com>
2026-02-16 01:36:35 -0800

    chore: bump to 0.5.6

--- packages/lpm-core/src/global.lua
+++ packages/lpm-core/src/global.lua
@@ -4,7 +4,7 @@ local fs = require("fs")
 local path = require("path")
 local process = require("process")
 
-global.currentVersion = "0.5.5"
+global.currentVersion = "0.5.6"
 
 ---@param s string
 local function sanitize(s)
