f3b7d69d93efaf408ddb93484266e9af07148260
David Cruz <david@codebycruz.com>
2026-02-19 00:09:12 -0800

    feat(compile): pass any generated .so or .dll files from /target/ into the compiled binary

--- packages/lpm-core/src/package/compile.lua
+++ packages/lpm-core/src/package/compile.lua
@@ -1,6 +1,11 @@
 local sea = require("sea")
 local fs = require("fs")
 local path = require("path")
+local process = require("process")
+
+local nativeExt = process.platform == "win32" and "dll"
+	or process.platform == "darwin" and "dylib"
+	or "so"
 
 ---@param package lpm.Package
 local function compilePackage(package)
@@ -9,7 +14,10 @@ local function compilePackage(package)
 
 	---@type table<{path: string, content: string}>
 	local files = {}
+	---@type table<{name: string, content: string}>
+	local sharedLibs = {}
 
+	---@param projectName string
 	---@param dir string
 	local function bundleDir(projectName, dir)
 		for _, relativePath in ipairs(fs.scan(dir, "**" .. path.separator .. "*.lua")) do
@@ -21,7 +29,7 @@ local function compilePackage(package)
 
 			-- Map file paths to Lua module names following the init.lua convention:
 			-- init.lua -> projectName, foo/init.lua -> projectName.foo, etc.
-			local moduleName = relativePath:gsub(path.separator, "."):gsub("%.lua$", ""):gsub("%.?init$", "")
+			local moduleName = string.gsub(relativePath, path.separator, "."):gsub("%.lua$", ""):gsub("%.?init$", "")
 			if moduleName ~= "" then
 				moduleName = projectName .. "." .. moduleName
 			else
@@ -30,19 +38,34 @@ local function compilePackage(package)
 
 			table.insert(files, { path = moduleName, content = content })
 		end
+
+		for _, relativePath in ipairs(fs.scan(dir, "**" .. path.separator .. "*." .. nativeExt)) do
+			local absPath = path.join(dir, relativePath)
+			local content = fs.read(absPath)
+			if not content then
+				error("Could not read file: " .. absPath)
+			end
+
+			-- Map e.g. "socket/core.so" -> "socket.core"
+			local moduleName = string.gsub(relativePath, path.separator, "."):gsub("%." .. nativeExt .. "$", "")
+			if moduleName ~= "" then
+				moduleName = projectName .. "." .. moduleName
+			else
+				moduleName = projectName
+			end
+
+			table.insert(sharedLibs, { name = moduleName, content = content })
+		end
 	end
 
 	local modulesDir = package:getModulesDir()
 	bundleDir(package:getName(), path.join(modulesDir, package:getName()))
 
-	-- Use the target directory for the build artifacts rather than src,
-	-- since build scripts may modify src contents.
 	for depName in pairs(package:getDependencies()) do
-		local buildFolder = path.join(modulesDir, depName)
-		bundleDir(depName, buildFolder)
+		bundleDir(depName, path.join(modulesDir, depName))
 	end
 
-	return sea.compile(package:getName(), files)
+	return sea.compile(package:getName(), files, sharedLibs)
 end
 
 return compilePackage
