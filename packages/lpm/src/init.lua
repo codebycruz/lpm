1df3bee6235eb39952320ae5c63fe09c933e627a
David Cruz <david@codebycruz.com>
2026-03-24 19:15:22 -0700

    fix(windows): enable utf8 console output on startup
    Fixes #60

--- packages/lpm/src/init.lua
+++ packages/lpm/src/init.lua
@@ -80,6 +80,12 @@ local fs = require("fs")
 
 local lpm = require("lpm-core")
 
+-- Enable UTF-8 console output on Windows
+if jit.os == "Windows" then
+	local win32 = require("winapi")
+	win32.kernel32.setConsoleOutputCP(win32.kernel32.ConsoleCP.UTF8)
+end
+
 lpm.global.init()
 
 local args = clap.parse({ ... })
