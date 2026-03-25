a00796e63ad1a543801051df1144171d0a3d150a
David Cruz <david@codebycruz.com>
2026-03-24 21:50:18 -0700

    tests(windows,arm64): skip shoddy test

--- install.ps1
+++ install.ps1
@@ -19,7 +19,6 @@ $rawArch = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
 $arch = if ($rawArch -eq "Arm64") { "aarch64" } else { "x86-64" }
 
 New-Item -ItemType Directory $dir -Force | Out-Null
-Write-Host "https://github.com/$repo/releases/download/$tag/lpm-windows-$arch.exe"
 Invoke-WebRequest "https://github.com/$repo/releases/download/$tag/lpm-windows-$arch.exe" -OutFile $bin
 
 & $bin --setup
